48c8897899d6f9d95239c978063c7c15c3f24289
Sharanbir Mander <smander@sandiego.edu>
2025-08-06 13:01:08 -0700

    Implemented int to roman

--- Leetcode.py
+++ Leetcode.py
@@ -59,3 +59,20 @@ class Solution:
                     r -= 1
                     
             return int(max_area) 
+        
+
+    class Solution:
+        def intToRoman(self, num: int) -> str:
+            # using list of lists instead of a dictionary because we want the iteration to be ordered
+            symbol_list = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9],["X", 10], ["XL", 40], ["L", 50], ["XC", 90], 
+                        ["C", 100], ["CD", 400], ["D", 500], ["CM", 900], ["M", 1000]]
+            
+            result = ""
+            
+            for sym, val in reversed(symbol_list):
+                if num // val:
+                    count = num // val
+                    result += (sym * count)
+                    num = num % val
+                    
+            return result
