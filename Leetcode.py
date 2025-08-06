65fcb91a06da6433a7ecf6fbafb06bf48f38a637
Sharanbir Mander <smander@sandiego.edu>
2025-08-06 16:52:05 -0700

    implemented roman to integer

--- Leetcode.py
+++ Leetcode.py
@@ -76,3 +76,19 @@ class Solution:
                     num = num % val
                     
             return result
+        
+
+    class Solution:
+        def romanToInt(self, s: str) -> int:
+            symbol_map = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000,}
+            
+            result = 0
+            
+            for i in range(len(s)):
+                if i + 1 < len(s) and symbol_map[s[i]] < symbol_map[s[i + 1]]:
+                    result -= symbol_map[s[i]]
+                    
+                else: 
+                    result += symbol_map[s[i]]
+            
+            return result
