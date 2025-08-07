b33c0534c85ee43dda14e657770c727eec954ccb
Sharanbir Mander <smander@sandiego.edu>
2025-08-07 13:02:49 -0700

    strStr()

--- Leetcode.py
+++ Leetcode.py
@@ -140,3 +140,16 @@ class Solution:
                     else:
                         r -= 1
             return int(closest)
+        
+    class Solution7:
+        def strStr(self, haystack: str, needle: str) -> int:
+            if needle == "":
+                return 0
+
+            for i in range(len(haystack) + 1 - len(needle)):
+                for j in range(len(needle)):
+                    if haystack[i + j] != needle[j]:
+                        break
+                    if j == len(needle) - 1:
+                        return i
+            return -1
