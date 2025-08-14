c97b7626baeee5e44a24b511029e426393cb46c8
Sharanbir Mander <smander@sandiego.edu>
2025-08-13 19:18:58 -0700

    LC 347

--- Leetcode.py
+++ Leetcode.py
@@ -354,3 +354,35 @@ class Solution:
             resultMap[key].append(word)
         
         return list(resultMap.values())
+    
+
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # initialize bucket as a list of empty lists
+        bucket = [[] for _ in range(len(nums) + 1)]
+        frequencyMap = {}
+
+        # Fill frequencyMap
+        for n in nums:
+            if n not in frequencyMap:
+                frequencyMap[n] = 1
+            else:
+                frequencyMap[n] += 1
+
+        # Fill The Bucket
+        for key, frequency in frequencyMap.items():
+            bucket[frequency].append(key)
+
+        result = []
+
+        # Iterate bucket in reverse to get top k frequent elements
+        for i in reversed(range(len(bucket))):
+            if bucket[i]:
+                for value in bucket[i]:
+                    if len(result) < k:
+                        result.append(value)
+                    else:
+                        return result
+        return result
