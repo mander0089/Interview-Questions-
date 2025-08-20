01ef6dfb343677cfc48de3b7b13ea45784799d70
Sharanbir Mander <smander@sandiego.edu>
2025-08-20 00:01:26 -0700

    LC 146

--- Leetcode.py
+++ Leetcode.py
@@ -430,3 +430,36 @@ class Solution:
                 else: 
                     l = m + 1
         return i
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dict = {}
+
+    def get(self, key: int) -> int:
+        if key in self.dict:
+            val = self.dict[key]
+            # move to end (most recent)
+            del self.dict[key]
+            self.dict[key] = val
+            return val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict:
+            # update and move to end
+            del self.dict[key]
+            self.dict[key] = value
+        else:
+            if len(self.dict) >= self.capacity:
+                # remove least recently used (first key)
+                oldest_key = next(iter(self.dict))   # O(1) for dict in Python 3.7+
+                del self.dict[oldest_key]
+            self.dict[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
