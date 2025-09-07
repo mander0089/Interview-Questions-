7d8b3a84653055afbcf62168e0ca08ddf7381e86
Sharanbir Mander <smander@sandiego.edu>
2025-09-07 09:21:38 -0700

    Inorder Preorder Tree Build

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -149,3 +149,34 @@ if __name__ == "__main__":
             result += 1
 
         return result
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        io_map = {}
+        for i in range(len(inorder)):
+            io_map[inorder[i]] = i
+
+        return self.splitTree(preorder, io_map, 0, 0, len(inorder) - 1)
+
+    def splitTree(self, preorder, io_map, rootIndex, left, right):
+        if left > right:
+            none
+
+        root = TreeNode(preorder[rootIndex])
+
+        mid = io_map[preorder[rootIndex]]
+
+        if mid > left:
+            root.left = self.splitTree(preorder, io_map, rootIndex + 1, left, mid -1)
+
+        if mid < right:
+            root.right = self.splitTree(preorder, io_map, rootIndex + mid - left + 1, mid + 1, right)
+
+        return root
